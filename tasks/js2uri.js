4d0e314866eda1e2bae4be69f5326ad42496c07d
Tom King <mobilemind@pobox.com>
2013-01-02 01:31:40 -0800

    added comments with to-do reminders

--- tasks/js2uri.js
+++ tasks/js2uri.js
@@ -15,13 +15,34 @@ module.exports = function(grunt) {
   // TASKS
   // ==========================================================================
 
-  // global var
+// TODO:
+// add options for--
+// protocol: 'javascript:', // default: 'javascript:'; set as needed. Might be used for custom iOS protocol schemes for instance
+// customVersion: 'value',  // default: ''; custom version string (supports "<jsu2ri:pkg.version>" and "<jsu2ri:meta.version>" tokens)
+// forceLastSemicolon: false, // default: false' if true forces a trailing semicolon (overrides noLastSemicolon)
+//
+// ** idea for another plugin **
+// html2data - convert HTML page to data URI
+//   eg,
+//  @printf "data:text/html;charset=utf-8;base64," > $(BUILDDIR)/$@
+//  @$(HTMLCOMPRESSOR) $(COMPRESSOPTIONSDATA) $(BUILDDIR)/$^ | base64 >> $(BUILDDIR)/$@
+//  Note: should have options for:
+//  protocol:	'data:',
+//  mimeType:	'text/html',
+//  charset:	'utf-8',
+//  encoding:	'base64',
+// *************************
+
+ // global var
   var jsURI_opt = {
+    protocol: 'javascript:',
     useNewlineEOL: true,
     useSingleQuote: true,
     appendVoid: true,
+    customVersion: '',
     appendVersion: false,
-    noLastSemicolon: true
+    noLastSemicolon: true,
+    forceLastSemicolon: false
   };
 
 
@@ -42,11 +63,14 @@ module.exports = function(grunt) {
       // get config options
       var configOptions = grunt.config(this.name+'.options');
       if (undefined !== configOptions) {
+        if (undefined !== configOptions.protocol)			jsURI_opt.protocol = configOptions.protocol;
         if (undefined !== configOptions.useNewlineEOL)		jsURI_opt.useNewlineEOL = configOptions.useNewlineEOL;
         if (undefined !== configOptions.useSingleQuote)		jsURI_opt.useSingleQuote = configOptions.useSingleQuote;
         if (undefined !== configOptions.appendVoid)			jsURI_opt.appendVoid = configOptions.appendVoid;
+        if (undefined !== configOptions.customVersion)		jsURI_opt.customVersion = configOptions.customVersion;
         if (undefined !== configOptions.appendVersion)		jsURI_opt.appendVersion = configOptions.appendVersion;
         if (undefined !== configOptions.noLastSemicolon)	jsURI_opt.noLastSemicolon = configOptions.noLastSemicolon;
+        if (undefined !== configOptions.forceLastSemicolon)	jsURI_opt.noLastSemicolon = configOptions.forceLastSemicolon;
       }
 
       // apply options to URI string
@@ -70,7 +94,7 @@ module.exports = function(grunt) {
   // javascript URI protocol + URI encoding
   grunt.registerHelper('js2uriString', function(jsString, newLineFlag) {
     var myEOL = newLineFlag ? '\n' : '\r\n';
-    return 'javascript:' + encodeURI(jsString.split(myEOL)[0]);
+    return jsURI_opt.protocol + encodeURI(jsString.split(myEOL)[0]);
   });
 
   // string replacements driven by config options
@@ -89,6 +113,7 @@ module.exports = function(grunt) {
         // use 0 *or* version from metadata *or* version from pkg (package.json)
         var pkgVersion = 0;
         if (uriOpts.appendVersion) {
+          if (undefined !== uriOpts.customVersion && '' !== uriOpts.customVersion) {} // do something with tokens
           if (undefined !== grunt.config('meta.version')) pkgVersion = grunt.config('meta.version');
           else if (undefined !== grunt.config('pkg.version')) pkgVersion = grunt.config('pkg.version');
         }
