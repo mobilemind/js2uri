1452beeb8457ca4df18643041fb3aa74123dba30
Tom King <mobilemind@pobox.com>
2013-01-08 20:15:59 -0800

    add javascript object syntax to test pattern, test pattern without protocol too

--- test/js2uriString.js
+++ test/js2uriString.js
@@ -26,7 +26,7 @@ exports['js2uri'] = {
     done();
   },
   'js2uriString encoding tests': function(test) {
-	test.expect(3);
+	test.expect(4);
 
     // test URI prefix
     test.deepEqual(grunt.helper('js2uriString', '', '', true),
@@ -39,10 +39,14 @@ exports['js2uri'] = {
 
     // test encoding of valid javascript with chars that are potentially an issue
     // i.e.: space, equal, bracket, double quote, comma, single quote, percent, semicolon, greater than, brace, plus
-    // unescaped test code is: var a=["0","1"],b='0%';if(a[1]>0){writeln("true"+b)}
-    test.deepEqual(grunt.helper('js2uriString', "var a=[\"0\",\"1\"],b='0%';if(a[1]>0){writeln(\"true\"+b)}", 'javascript:', true),
-      "javascript:var%20a=%5B%220%22,%221%22%5D,b='0%25';if(a%5B1%5D%3E0)%7Bwriteln(%22true%22+b)%7D",
+    // unescaped test code is: var a=["0","1"],b='0%',c={p:0};if(a[1]>0){writeln("true"+b)}
+    test.deepEqual(grunt.helper('js2uriString', "var a=[\"0\",\"1\"],b='0%',c={p:0};if(a[1]>0){writeln(\"true\"+b)}", 'javascript:', true),
+      "javascript:var%20a=%5B%220%22,%221%22%5D,b='0%25',c=%7Bp:0%7D;if(a%5B1%5D%3E0)%7Bwriteln(%22true%22+b)%7D",
       "js2uriString {test code} should return- javascript:var%20a=%5B%220%22,%221%22%5D,b='0%25';if(a%5B1%5D%3E0)%7Bwriteln(%22true%22+b)%7D");
+
+	  test.deepEqual(grunt.helper('js2uriString', "var a=[\"0\",\"1\"],b='0%',c={p:0};if(a[1]>0){writeln(\"true\"+b)}", '', true),
+      "var%20a=%5B%220%22,%221%22%5D,b='0%25',c=%7Bp:0%7D;if(a%5B1%5D%3E0)%7Bwriteln(%22true%22+b)%7D",
+      "js2uriString {test code} should return- var%20a=%5B%220%22,%221%22%5D,b='0%25';if(a%5B1%5D%3E0)%7Bwriteln(%22true%22+b)%7D");
     test.done();
   }
 };
